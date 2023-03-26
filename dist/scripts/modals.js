4abb3b220cba0454d1ed0c4e1218d091f7546718
GavG <gavin.gration@vaioni.com>
2023-03-26 18:02:08 +0100

    scroll to top on form submit

--- dist/scripts/modals.js
+++ dist/scripts/modals.js
@@ -3,15 +3,22 @@ const modal = {
     init: () => {
         modal.initOpeners()
         modal.initClosers()
+        modal.initFrameWatchers()
     },
 
     initOpeners: () => document.querySelectorAll('.modal-btn').forEach(
-        (btn) => modal.addBtnOpenerListener(btn, btn.dataset.modalTarget)
+        btn => modal.addBtnOpenerListener(btn, btn.dataset.modalTarget)
     ),
 
     initClosers: () => document.querySelectorAll('.modal-closer').forEach(
-        (btn) => modal.addBtnCloserListener(btn, btn.dataset.modalTarget)
+        btn => modal.addBtnCloserListener(btn, btn.dataset.modalTarget)
     ),
+
+    initFrameWatchers: () => document.querySelectorAll('iframe').forEach(
+        frame => frame.addEventListener('load', modal.frameLoaded)
+    ),
+
+    frameLoaded: loadEvent => window.scrollTo(0, 0),
 
     addBtnOpenerListener: (btn, modalTarget) => btn.addEventListener(
         'click',
@@ -23,13 +30,13 @@ const modal = {
         () => modal.closeModal(modalTarget)
     ),
 
-    showModal: (modalId) => {
+    showModal: modalId => {
         document.querySelector(modalId).classList.add('show')
         window.activeModalId = modalId
         window.scrollTo(0, 0)
     },
 
-    closeModal: (modalId) => {
+    closeModal: modalId => {
         document.querySelector(modalId).classList.remove('show')
         delete window.activeModalId
         window.scrollTo(0, 0)
