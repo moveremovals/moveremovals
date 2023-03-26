6cfe26766aa156f01affd9567bb2baa2996f0bf7
GavG <gavin.gration@vaioni.com>
2023-03-26 12:46:02 +0100

    modal quote

--- dist/scripts/modals.js
+++ dist/scripts/modals.js
@@ -3,19 +3,55 @@ const modal = {
     init: () => {
         modal.initOpeners()
         modal.initClosers()
+        modal.initFfListeners()
     },
 
     initOpeners: () => document.querySelectorAll('.modal-btn').forEach(
-        (btn) => btn.addEventListener('click', () => modal.showModal(btn.dataset.modalTarget))
+        (btn) => modal.addBtnOpenerListener(btn, btn.dataset.modalTarget)
     ),
 
     initClosers: () => document.querySelectorAll('.modal-closer').forEach(
-        (btn) => btn.addEventListener('click', () => modal.closeModal(btn.dataset.modalTarget))
+        (btn) => modal.addBtnCloserListener(btn, btn.dataset.modalTarget)
     ),
 
-    showModal: (modalId) => document.querySelector(modalId).classList.add('show'),
+    addBtnOpenerListener: (btn, modalTarget) => btn.addEventListener(
+        'click',
+        () => modal.showModal(modalTarget)
+    ),
 
-    closeModal: (modalId) => document.querySelector(modalId).classList.remove('show'),
+    addBtnCloserListener: (btn, modalTarget) => btn.addEventListener(
+        'click',
+        () => modal.closeModal(modalTarget)
+    ),
+
+    initFfListeners: () => {
+        const ffForm = document.getElementById('ff-compose')
+        const observer = new MutationObserver(() => {
+            let ffSuccess = document.getElementById('ff-success')
+
+            if (ffSuccess) {
+                let dismissFFBtn = document.createElement('button')
+                dismissFFBtn.dataset.modalTarget = window.activeModalId
+                dismissFFBtn.classList.add('modal-closer-btn')
+                dismissFFBtn.innerText = 'Dismiss'
+                modal.addBtnCloserListener(dismissFFBtn, window.activeModalId)
+                ffSuccess.parentElement.appendChild(dismissFFBtn)
+            }
+        })
+        observer.observe(ffForm, {childList: true})
+    },
+
+    showModal: (modalId) => {
+        document.querySelector(modalId).classList.add('show')
+        document.body.classList.add('modal-showing')
+        window.activeModalId = modalId
+    },
+
+    closeModal: (modalId) => {
+        document.querySelector(modalId).classList.remove('show')
+        document.body.classList.remove('modal-showing')
+        delete window.activeModalId
+    }
 
 }
 
